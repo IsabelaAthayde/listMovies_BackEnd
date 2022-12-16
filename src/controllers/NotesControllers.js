a04685dce2625481a157ca0d0d22c2fef487a3d2
Isabela de Oliveira Athayde <isabelaoa15@gmail.com>
2022-12-15 21:01:37 -0300

    added notes controllers

--- src/controllers/NotesControllers.js
+++ src/controllers/NotesControllers.js
@@ -1,4 +1,69 @@
-const { hash, compare } = require('bcryptjs');
-const AppError = require("../utils/AppError");
+const knex = require('../database/knex');
+const sqliteConnection = require("../database/sqlite/");
+const AppError = require('../utils/AppError');
 
-const sqliteConnection = require("../database/sqlite/")
+
+class NotesControllers {
+    async create(request, response) {
+        const { title, description, rating, tags} = request.body;
+        const { user_id } = request.params;
+
+        const database = await sqliteConnection();
+        const movieNotesExists = await database.get("SELECT * FROM movieNotes WHERE title = (?) ", [title])
+
+        if(movieNotesExists) {
+            throw new AppError(`Já existe uma nota do filme ${title}`)
+        }
+
+        const note_id = await knex("movieNotes").insert({
+            title,
+            description,
+            rating,
+            user_id
+        })
+
+        const tagsInsert = tags.map(name => {
+            return {
+                note_id,
+                user_id,
+                name
+            }
+        })
+
+        await knex("tags").insert(tagsInsert)
+    
+        response.json();
+    }
+
+    async show(request, response) {
+        const { id } = request.params;
+
+        const note = await knex("movieNotes").where({ id }).first();
+        const tags = await knex("tags").where({note_id: id}).orderBy("name");
+        return response.json({
+            ...note, 
+            tags})
+    }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("movieNotes").where({ id }).delete();
+        return response.json();
+    }
+
+    async index(request, response) {
+        const { title, user_id, tags } = request.query;
+
+       
+
+           const notes = await knex("movieNotes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)
+            .orderBy("title");
+       
+        return response.json(notes)
+    }
+}
+
+module.exports = NotesControllers;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 
 const userRouter = require('./users.routes');
+const notesRouter = require('./notes.routes');
+
 
 const routes = Router();
 
 routes.use('/users', userRouter)
+routes.use('/movieNotes', notesRouter)
+
 
 module.exports = routes;
